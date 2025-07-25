9b692568546b73487e44ce3e513a5a512fe0aaa5
Sofiia13 <sophiyastanish@gmail.com>
2025-07-25 17:19:09 +0300

    hide footer

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,9 @@ export const App: React.FC = () => {
         <TodoList todos={filteredTodos} />
 
         {/* Hide the footer if there are no todos */}
-        <TodoFooter todos={todos} filter={filter} setFilter={setFilter} />
+        {todos && todos.length > 0 && (
+          <TodoFooter todos={todos} filter={filter} setFilter={setFilter} />
+        )}
       </div>
 
       {/* DON'T use conditional rendering to hide the notification */}
